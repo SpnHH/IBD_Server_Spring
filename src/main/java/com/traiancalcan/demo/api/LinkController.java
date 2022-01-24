92035ca69476442ed5be32ac6f96a600cb076ca7
SpnHH <65242527+SpnHH@users.noreply.github.com>
2022-01-24 15:19:11 +0200

    GetLinksById return a list of String now

--- src/main/java/com/traiancalcan/demo/api/LinkController.java
+++ src/main/java/com/traiancalcan/demo/api/LinkController.java
@@ -9,7 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RequestMapping("api/v1/link")
 @RestController
@@ -24,11 +26,37 @@ public class LinkController {
         this.linkService = linkService;
     }
 
+//    @GetMapping(path ="{name}")
+//    public List<Link> getLinksByName(@PathVariable("name") String name){
+//        Optional<Person> p = personService.getPersonByName(name);
+//        if(p.isEmpty()){
+//            return null;
+//        }
+//        return linkService.getLinkByPersonLink(p.get().getId());
+//    }
+
     @GetMapping(path ="{name}")
-    public List<Link> getLinksByName(@PathVariable("name") String name){
-        Person p = personService.getPersonByName(name).get();
-        return linkService.getLinkByPersonLink(p.getId());
+    @ResponseBody
+    public List<String> getLinksByName(@PathVariable("name") String name){
+        List<String> retList = new ArrayList<>();
+        Optional<Person> p = personService.getPersonByName(name);
+        if(p.isEmpty()){
+            return null;
+        }
+        var links = linkService.getLinkByPersonLink(p.get().getId());
+        for(var link:links){
+            var x1 = personService.getPersonById(link.getPerson1());
+            var x2 = personService.getPersonById(link.getPerson2());
+            if(x1.get().getName().equals(p.get().getName())){
+                retList.add(x2.get().getName());
+            }else{
+                retList.add(x1.get().getName());
+            }
+        }
+        return retList;
     }
+
+
     @PostMapping
     public void addLink(@NonNull @RequestBody LinkByName link){
         Person p1 = personService.getPersonByName(link.getName1()).get();
